93e8aee4ef07c2baf1ff4a9ca9a390d675150eae
I. <40337+israellevin@users.noreply.github.com>
2026-09-20 15:36:05 +0300

    Split niriu.sh once more

--- ned/examples/noempty.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/sh -e
-[ "$NED_PID" ] || exec ned WindowClosed "$0"
-get() { niri msg --json "$1" | jq -r "$2"; }
-workspace_id="$(get workspaces '.[] | select(.is_focused == true) | .id')"
-[ "$workspace_id" ] || exit 0
-num_of_windows="$(get windows '[.[] | select(.workspace_id == '"$workspace_id"') ] | length')"
-[ "$num_of_windows" = 0 ] && niri msg action focus-window-previous
--- ned/examples/showlayout.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/sh -e
-[ "$NED_PID" ] || exec ned KeyboardLayoutSwitched "$0"
-case $(jq -r '.KeyboardLayoutSwitched.idx') in
-  0) niriu.sh conf --rm 'layout { border { on; width 4; active-color "blue"; }; }';;
-  *) niriu.sh conf --add 'layout { border { on; width 4; active-color "blue"; }; }';;
-esac
